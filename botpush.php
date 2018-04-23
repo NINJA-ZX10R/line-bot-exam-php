c608274e32de6e13397c8d9ca9eb4c626701f28a
NINJA-ZX10R <weerasak.c2@gmail.com>
2018-04-23 12:55:35 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = '+4eVXou/NR4uXTDUmpE0SdqDwFMOfWgUHlBDjA11FQdDi1Tzz8eQd2RWrG9T0ep
 
 $channelSecret = '8bdcd1da660c6b0b23d47fdcf500c23a';
 
-$pushID = 'Ua58afb3a29de69ba7d8d9ff6ad427d33';
+$pushID = 'Uf2f1f57c5e3554a925621e155e252ae0';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
